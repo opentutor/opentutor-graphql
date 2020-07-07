49a22a266594e3ac78b862ebdd5757e6fb63bef0
kcarr <kcarr@ict.usc.edu>
2020-07-07 12:50:22 -0700

    Calculate and set UserSession score and Session grade when setGrade is called

--- node/src/graphql/mutation/index.ts
+++ node/src/graphql/mutation/index.ts
@@ -1,13 +1,11 @@
 import { GraphQLObjectType } from 'graphql';
 import setGrade from './set-grade';
-import setSessionGrade from './set-session-grade';
 import updateSession from './update-session';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: {
     setGrade,
-    setSessionGrade,
     updateSession,
   },
 });
