39efd4a727904b56823ed7d17e357b3f610d52dc
kcarr <kcarr@ict.usc.edu>
2020-07-27 12:38:20 -0700

    Grading only requires 1 grade per user response
    Grading calculates score per expectation, then average score for expectations

--- node/src/models/utils/calculate-score.ts
+++ node/src/models/utils/calculate-score.ts
@@ -4,26 +4,41 @@ const calculateScore = (
   userSession: UserSession,
   gradeField = 'graderGrade'
 ) => {
+  const expGrades = [];
   let score = 0;
-  let numExpectations = 0;
+
   for (let i = 0; i < userSession.userResponses.length; i++) {
     const userResponse = userSession.userResponses[i];
-    numExpectations += userResponse.expectationScores.length;
+    let isGraded = false;
     for (let j = 0; j < userResponse.expectationScores.length; j++) {
       const expectationScore = userResponse.expectationScores[j] as any;
-      if (!expectationScore[gradeField]) {
-        return null;
+      if (expectationScore[gradeField]) {
+        isGraded = true;
+        if (!expGrades[j]) {
+          expGrades[j] = [];
+        }
+        expGrades[j].push(expectationScore[gradeField]);
       }
-      const val =
-        expectationScore[gradeField] === 'Good'
-          ? 1
-          : expectationScore[gradeField] === 'Neutral'
-          ? 0.5
-          : 0;
-      score += val;
+    }
+    // each response needs at least one grade
+    if (!isGraded) {
+      return null;
     }
   }
-  return score / numExpectations;
+
+  expGrades.forEach((expectation: [string]) => {
+    let expectationScore = 0;
+    expectation.forEach((grade: string) => {
+      if (grade === 'Good') {
+        expectationScore += 1;
+      } else if (grade === 'Neutral') {
+        expectationScore += 0.5;
+      }
+    });
+    score += expectationScore / expectation.length;
+  });
+
+  return score / expGrades.length;
 };
 
 export default calculateScore;
