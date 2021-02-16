b2ac3f4415394793a4e0a2093103e4e222e5f19e
auerbach <auerbach@ict.usc.edu>
2021-02-16 14:55:46 -0800

    compacted code.

--- node/src/gql/query/find-one.ts
+++ node/src/gql/query/find-one.ts
@@ -69,10 +69,13 @@ export function findOne<T>(config: {
         },
         {}
       );
-      const filter = Object.assign({}, mArgs, {
-        $or: [{ deleted: false }, { deleted: null }],
-      });
-      const item = await model.findOne(filter).exec();
+      //const filter = Object.assign({}, mArgs, {
+      //  $or: [{ deleted: false }, { deleted: null }],
+      //});
+
+      const item = await model
+        .findOne({ deleted: { $ne: true }, ...mArgs })
+        .exec();
       if (!item && !disableExceptionOnNotFound) {
         throw new Error(
           `${typeName} not found for args "${JSON.stringify(args)}"`
