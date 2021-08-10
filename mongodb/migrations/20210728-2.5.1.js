ac04dba778bed908aba658d4ee4ea7d7a5d21930
Daniel Budziwojski <daniel.budziwojski@gmail.com>
2021-08-10 11:39:11 -0700

    Revert migration to iterative

--- mongodb/migrations/20210728-2.5.1.js
+++ mongodb/migrations/20210728-2.5.1.js
@@ -3,34 +3,31 @@ module.exports = {
   async up(db, client) {
     console.log(`migrate up`);
 
-    // async function getCollection(collectionName) {
-    //   const result = []
-    //   await db.collection(collectionName).find({}).forEach(item => {
-    //     result.push(item)
-    //   })
-    //   return result
-    // }
+    async function getCollection(collectionName) {
+      const result = []
+      await db.collection(collectionName).find({}).forEach(item => {
+        result.push(item)
+      })
+      return result
+    }
 
-    // const lessons =  await getCollection("lessons");
+    const lessons =  await getCollection("lessons");
 
-    // lessons.forEach(lesson => {
-    //   // if(lesson.image) {
-    //   //   lesson.media = {
-    //   //     url: lesson.image,
-    //   //     type: "image",
-    //   //     props: []
-    //   //   } 
-    //   // }
-    //     // lesson.media = null;
-    // })
+    lessons.forEach(lesson => {
+      if(lesson.image) {
+        lesson.media = {
+          url: lesson.image,
+          type: "image",
+          props: null
+        } 
+      } else {
+        lesson.media = null;
+      }
+    })
 
-    // lessons.forEach(lesson => {
-    //   db.collection("lessons").updateOne({lessonId: lesson.lessonId}, {$set:lesson});
-    // })
-
-    db.collection("lessons").updateMany(
-      {image: {$exists:true, $ne:"" }}, { $set: { media: { url: $image, type: "image" } } }
-    )
+    lessons.forEach(lesson => {
+      db.collection("lessons").updateOne({lessonId: lesson.lessonId}, {$set:lesson});
+    })
    
   },
 
